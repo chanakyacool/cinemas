173c576bbf50302af458e26fc7cf7f67b51ec900
Kishan Kabra <kishan-mj@kishanmj-H61MLV.(none)>
2014-07-18 17:06:12 +0530

    contr image

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,7 @@
 class MoviesController < ApplicationController
 	def movie 
 	  @imdb = ImdbParty::Imdb.new(:anonymize => true)
-	  @top_250 = @imdb.top_250.first
-	  @i = Imdb::Movie.new("#{@top_250[:imdb_id]}")
+	  @top_250 = @imdb.top_250.first 
 	end
 
 	def search
